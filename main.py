0b92b45d07fe54330cd497d66ea89291b4b7408a
Pet Family Developer <petfamily@example.com>
2026-02-25 22:14:32 +0700

    chore: Ignore more non-frontend files

--- main.py
+++
@@ -1,5 +0,0 @@
-from gui import VetAdminGUI
-
-if __name__ == "__main__":
-    app = VetAdminGUI()
-    app.mainloop()
--- run_tests.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-with open('test_output.txt', 'w') as f:
-    subprocess.run(['python', '-m', 'pytest', 'tests/admin_tests/'], stdout=f, stderr=subprocess.STDOUT)
